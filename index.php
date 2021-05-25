b46605125e481a5f701d1059db09cc7b217b070e
Narindra91 <67949236+Narindra91@users.noreply.github.com>
2021-05-25 06:39:51 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+      <head>
+        <title>Web project</title>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <div class="corps_page">
+        </div>
+      </body>
+</html>
