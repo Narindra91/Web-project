01ab423be3b23c27f722a80f4f18d7461059be90
Narindra <rasolofonjatovo91@gmail.com>
2021-06-11 10:17:55 -0300

    premier commit

--- index.php
+++ index.php
@@ -1,31 +1,10 @@
 <!DOCTYPE html>
-<html>
-      <head>
-        <title>Web project</title>
-        <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body>
-      	<div class="corps_page">
-        	<div class="entete_page">
-        		<div class="logo">
-        			<i class="fa fa-accusoft"></i>
-        			<span>Accuria</span>
-        		</div>
-        		<div>
-        		 	<ul class="nav justify-content-end">
-          				<li class="nav-item"><a class="nav-link" href="#">Cours</a></li>
-          				<li class="nav-item"><a class="nav-link" href="#">Articles</a></li>
-          				<li class="nav-item"><a class="nav-link" href="#">Soutien</a></li>
-          				<li class="nav-item"><a class="nav-link " href="#">Contact</a></li>
-          			</ul>
-        		</div>
-            <div class="test">
-            </div>
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js">
-      	</script>
-      </body>
+	<head>
+		<title>Web Project</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<h1>Projet web pour le Git</h1>
+	</body>
 </html>
